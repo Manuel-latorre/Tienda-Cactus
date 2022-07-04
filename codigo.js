d73f626610d8b1770aa62a68889e8ea286e37a8f
Manuel Latorre <manuel.latorre11@gmail.com>
2022-07-04 18:27:20 -0300

    ciclos/iteraciones

--- codigo.js
+++ codigo.js
@@ -1,24 +1,17 @@
 
 
 
-
-
-ingresarResultado = prompt("¿Cuanto es 3x3?")
-if (ingresarResultado == 9){
-    alert("sos un crack")
-} else{
-    alert(" Resultado incorrecto,a practicar las tablas broder")
-} 
-
-
 let ingresarNumero = parseInt (prompt("Ingrese un numero"));
 
-for (let i = 1; i < 25; i++){
+for (let i = 1; i < 20; i++){
     let resultado = ingresarNumero + i;
     alert (ingresarNumero +"+"+ i +"="+ resultado);
     if (resultado == 10){
         break;
     }
-}
+} 
+
+
+
 
 
