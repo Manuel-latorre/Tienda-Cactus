d4f1cbd63cf0e74ddcfe6b2f8aa3b7a9680a745e
Manuel Latorre <manuel.latorre11@gmail.com>
2022-07-12 17:02:39 -0300

    simulador interactivo

--- codigo.js
+++ codigo.js
@@ -1,84 +1,5 @@
-
-let producto1 = "Zapatillas Nike";
-let precio1 = 560;
-
-let producto2 = "Buzo Puma";
-let precio2 = 340;
-
-let nombre = ""
-
-
-function solicitarNombre(){
-    nombre = prompt("¡Bienvenido a Tienda Cactus!. Ingrese su nombre por favor");
-    
-}
-
-function validarPrecio(precio1, precio2){
-    let elegirProducto = prompt("¡Hola"+ " " + nombre + "!"+ " " +"elija con el numero correspondiente el producto que desea añadir al carrito: "+ "\n1)" + producto1 +" "+ "$"+" " +precio1 + "\n2)" + producto2 + " "+"$"+" " + precio2);{
-        if (elegirProducto == 1){
-        alert("Ha agregado al carrito" + " " + producto1 + ", el precio final de la compra es de " + "$" + precio1);
-        }
-        else if(elegirProducto == 2){
-            alert("Ha agregado al carrito" + " " + producto2 + ", el precio final de la compra es de " + "$" + precio2);
-        }
-        
-    }
-}
-
-function cobrarProducto(precio1, precio2){
-    let pago = parseInt(prompt("¿Con cuanto dinero va a pagar?"));
-    if (pago >= precio1){
-        alert("¡Muchas gracias por su compra!. Su vuelto es de: "+ "$" + (pago - precio1));
-    }else if (pago >= precio2){
-        alert("¡Muchas gracias por su compra!. Su vuelto es de: "+ "$" + (pago - precio2));
-    }else{
-        alert("No le alcanza el dinero");
-    }
-
-
-
-}
-
-solicitarNombre()
-validarPrecio(precio1, precio2)
-cobrarProducto(precio1, precio2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* class Producto{
+/* Clase constructora */
+class Producto{
     constructor(nombre, talle, precio, stock){
         this.nombre = nombre;
         this.talle = talle;
@@ -95,7 +16,56 @@ const producto4 = new Producto("Nike Jordan 1 low", 39, 615, 3);
 const producto5 = new Producto("Hoodie Cactus Jack", "M", 219, 4);
 const producto6 = new Producto("Remera Kaws", "S", 90, 4);
 const producto7 = new Producto("Remera McQueen", "XL", 75, 2);
-const producto8 = new Producto("Crewneck Supreme", "L", 235, 5);  */
+const producto8 = new Producto("Crewneck Supreme", "L", 235, 5); 
+
+
+
+/* Variables que utilizo dentro del simulador */
+
+let totalCarrito = 1;
+let precio = 1;
+let respuesta;
+
+
+
+/* Simulador */
+
+
+for (let intentos = 0; intentos <= 2; intentos++) {
+    let contraseña = parseInt(prompt('Ingrese su clave'));
+    if (contraseña === 321){
+            alert("Elija el producto que quiera añadir al carrito");
+            function agregarAlCarrito(Producto){
+                console.log("Agregaste al carrito: " + Producto + " " + "$" + precio);
+                console.log("Total productos: " + totalCarrito++);
+                console.log("Precio Total: " + precio++);
+                if (totalCarrito === 4){
+                    prompt("Su carrito ya esta lleno, ¿Desea finalizar la compra?");
+                    if (respuesta === "si" && respuesta === "Si");{
+                    alert("El precio total de su compra es de: " + " "+ "$" + precio);
+                    }
+        
+                    let pago = parseInt(prompt("¿Con cuanto dinero paga?"));{
+                        if(pago >= precio){
+                            alert("Gracias por su compra, su vuelto es de: " + "$" + (pago - precio));
+                            }
+                            else{
+                                alert("No le alcanza el dinero")
+                            }
+                    }
+                }
+            }
+            break;
+        }
+        else{
+            alert("Contraseña incorrecta");
+    }
+    
+    if (intentos === 2){
+        alert("Intentos agotados, vuelva pronto")
+    }
+}   
+
 
 
 
