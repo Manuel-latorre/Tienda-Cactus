f0210b8c77504cd024ea9521bb09fcd2fe9a6d5c
Manuel Latorre <manuel.latorre11@gmail.com>
2022-08-23 16:48:38 -0300

    probando fetch

--- codigo.js
+++ codigo.js
@@ -1,3 +1,4 @@
+
 /* array de los productos */
 
 const productos = [
@@ -47,7 +48,53 @@ const productos = [
     {id: 49, nombre:"Remera Off White Ld", precio: 28500, img: "../imagenesIndex/imagenes-remeras/remeraoffwhitemangalarga.webp", category: 'Remeras', marca: 'Off White', color: 'blanco', descripcion:"Esta Graphic Set-In Neck es suave y cómoda, esta prenda básica está hecha con un corte relajado. Combina esta remera con tus jeans favoritos y dale un renovado giro a tu look.<br> Composicion: 100% poliester <br> Origen: China <br> Calce: SS TEES GRAPHICS", sku:"SKU: BST-377"},
     {id: 50, nombre:"Remera Palm Angels Bear", precio: 28500, img: "../imagenesIndex/imagenes-remeras/remerapalmangelsnegra.webp", category: 'Remeras', marca: 'Palm Angels', color: 'negro', descripcion:"Esta Graphic Set-In Neck es suave y cómoda, esta prenda básica está hecha con un corte relajado. Combina esta remera con tus jeans favoritos y dale un renovado giro a tu look.<br> Composicion: 100% poliester <br> Origen: China <br> Calce: SS TEES GRAPHICS", sku:"SKU: BST-669"},
     {id: 51, nombre:"Remera Sun of Beach", precio: 28500, img: "../imagenesIndex/imagenes-remeras/remerasunofbeach.webp", category: 'Remeras', marca: 'Palm Angels', color: 'negro', descripcion:"Esta Graphic Set-In Neck es suave y cómoda, esta prenda básica está hecha con un corte relajado. Combina esta remera con tus jeans favoritos y dale un renovado giro a tu look.<br> Composicion: 100% poliester <br> Origen: China <br> Calce: SS TEES GRAPHICS", sku:"SKU: BST-765"},  
+    {id: 52, nombre:"Vans Authentic 44", precio: 36999, img: "./imagenes/imagenes-zapatillas/vansauthentic44.webp", category: 'Zapatillas', marca: 'Vans', modelo:'vans' , color: 'amarillo', sku:"SKU: BST-761"},
+    {id: 53, nombre:"Vans Authentic Bandana", precio: 22999, img: "./imagenes/imagenes-zapatillas/vansauthenticbandana.jpg", category: 'Zapatillas', marca: 'Vans', modelo:'vans' , color: 'blanco', sku:"SKU: BSW-749"},
+    {id: 54, nombre:"Vans Old Skool Navy", precio: 19999, img: "./imagenes/imagenes-zapatillas/vansazules.webp", category: 'Zapatillas', marca: 'Vans', modelo:'vans' , color: 'azul', sku:"SKU: BST-122"},
+    {id: 55, nombre:"Vans Old Skool Bandana", precio: 16999, img: "./imagenes/imagenes-zapatillas/vansoldschoolbandana.webp", category: 'Zapatillas', marca: 'Vans', modelo:'vans' , color: 'marron', sku:"SKU: BST-777"},
+    {id: 56, nombre:"Vans Old Skool Race", precio: 23999, img: "./imagenes/imagenes-zapatillas/vansoldskoolrace.webp", category: 'Zapatillas', marca: 'Vans', modelo:'vans' , color: 'blanco', sku:"SKU: BST-599"},
+    {id: 57, nombre:"Vans Sk8 Hi Navy", precio: 25999, img: "./imagenes/imagenes-zapatillas/vanssk8hiazules.webp", category: 'Zapatillas', marca: 'Vans', modelo:'vans' , color: 'azul', sku:"SKU: BST-229"},
+    {id: 59, nombre:"Vans Sk8 Hi Brown", precio: 20999, img: "./imagenes/imagenes-zapatillas/vanssk8himarron.webp", category: 'Zapatillas', marca: 'Vans', modelo:'vans' , color: 'marron', sku:"SKU: BST-119"},
+    {id: 60, nombre:"Jordan 1 Dior", precio: 60999, img: "./imagenes/imagenes-zapatillas/jordan1dior.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'gris', sku:"SKU: BST-449"},
+    {id: 61, nombre:"Jordan 1 Fearless", precio: 56799, img: "./imagenes/imagenes-zapatillas/jordan1fearless.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'blanco', sku:"SKU: BST-118"},
+    {id: 62, nombre:"Jordan 1 Gold", precio: 49999, img: "./imagenes/imagenes-zapatillas/jordan1gold.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'amarillo', sku:"SKU: BST-133"},
+    {id: 63, nombre:"Jordan 1 Green", precio: 58999, img: "./imagenes/imagenes-zapatillas/jordan1green.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'verde', sku:"SKU: BST-988"},
+    {id: 64, nombre:"Jordan 1 GreyFrog", precio: 66999, img: "./imagenes/imagenes-zapatillas/jordan1greyfog.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'gris', sku:"SKU: BST-101"},
+    {id: 65, nombre:"Jordan 1 Mid", precio: 59999, img: "./imagenes/imagenes-zapatillas/jordan1mid.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'azul', sku:"SKU: BST-103"},
+    {id: 66, nombre:"Jordan 1 Se", precio: 44999, img: "./imagenes/imagenes-zapatillas/jordan1se.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'amarillo', sku:"SKU: BST-100"},
+    {id: 67, nombre:"Jordan 1 Snake", precio: 42999, img: "./imagenes/imagenes-zapatillas/jordan1snake.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'negro', sku:"SKU: BST-818"},
+    {id: 68, nombre:"Jordan 1 Unc", precio: 66999, img: "./imagenes/imagenes-zapatillas/jordan1unc.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan1' , color: 'celeste', sku:"SKU: BST-434"},
+    {id: 69, nombre:"Jordan 3 Green", precio: 50999, img: "./imagenes/imagenes-zapatillas/jordan3green.webp", category: 'Zapatillas', marca: 'Jordan', color: 'verde', sku:"SKU: BST-766"},
+    {id: 70, nombre:"Jordan 3 Patchwork ", precio: 49999, img: "./imagenes/imagenes-zapatillas/jordan3patchwork.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan3' , color: 'marron', sku:"SKU: BST-677"},
+    {id: 71, nombre:"Jordan 3 Red", precio: 52999, img: "./imagenes/imagenes-zapatillas/jordan3red.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan3' , color: 'rojo', sku:"SKU: BST-676"},
+    {id: 72, nombre:"Jordan 3 Red Retro", precio: 56999, img: "./imagenes/imagenes-zapatillas/jordan3redretro.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan3' , color: 'rojo', sku:"SKU: BST-686"},
+    {id: 73, nombre:"Jordan 3 Unc", precio: 42999, img: "./imagenes/imagenes-zapatillas/jordan3unc.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan3' , color: 'celeste', sku:"SKU: BST-556"},
+    {id: 74, nombre:"Jordan 3 Blue", precio: 52999, img: "./imagenes/imagenes-zapatillas/jordan3blue.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan3' , color: 'azul', sku:"SKU: BST-221"},
+    {id: 75, nombre:"Jordan 3 Black", precio: 64899, img: "./imagenes/imagenes-zapatillas/jordan3black.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan3' , color: 'negro', sku:"SKU: BST-176"},
+    {id: 76, nombre:"Jordan 3 Chicago", precio: 51999, img: "./imagenes/imagenes-zapatillas/jordan3chicago.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan3' , color: 'rojo', sku:"SKU: BST-233"},
+    {id: 77, nombre:"Jordan 3 Chicago Retro", precio: 58999, img: "./imagenes/imagenes-zapatillas/jordan3chicagoretro.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan3' , color: 'rojo', sku:"SKU: BST-322"},
+    {id: 78, nombre:"Jordan 4 Bred", precio: 65999, img: "./imagenes/imagenes-zapatillas/jordan4bred.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'negro', sku:"SKU: BST-155"},
+    {id: 79, nombre:"Jordan 4 Chicago", precio: 61999, img: "./imagenes/imagenes-zapatillas/jordan4chicago.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'rojo', sku:"SKU: BST-255"},
+    {id: 80, nombre:"Jordan 4 Kaws", precio: 66999, img: "./imagenes/imagenes-zapatillas/jordan4kaws.jpg", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'gris', sku:"SKU: BST-355"},
+    {id: 81, nombre:"Jordan 4 Mocha", precio: 59999, img: "./imagenes/imagenes-zapatillas/jordan4moha.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'marron', sku:"SKU: BST-455"},
+    {id: 82, nombre:"Jordan 4 PureMoney", precio: 47999, img: "./imagenes/imagenes-zapatillas/jordan4puremoney.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'blanco', sku:"SKU: BST-555"},
+    {id: 83, nombre:"Jordan 4 Purwhite", precio: 44999, img: "./imagenes/imagenes-zapatillas/jordan4purwhite.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'blanco', sku:"SKU: BST-655"},
+    {id: 84, nombre:"Jordan 4 Unc", precio: 70999, img: "./imagenes/imagenes-zapatillas/jordan4unc.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'celeste', sku:"SKU: BST-575"},
+    {id: 85, nombre:"Jordan 4 White Oreo", precio: 48999, img: "./imagenes/imagenes-zapatillas/jordan4whiteoreo.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'blanco', sku:"SKU: BST-755"},
+    {id: 86, nombre:"Jordan 4 X Levis Classic", precio: 39999, img: "./imagenes/imagenes-zapatillas/jordan4xlevis.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'Jordan4' , color: 'celeste', sku:"SKU: BST-855"},
+    {id: 87, nombre:"Nike Dunk Goldenroad", precio: 44999, img: "./imagenes/imagenes-zapatillas/nikedunkgoldenroad.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'NikeDunk' , color: 'amarillo', sku:"SKU: BST-001"},
+    {id: 88, nombre:"Nike Dunk High Brazil", precio: 38999, img: "./imagenes/imagenes-zapatillas/nikedunkhighbrasil.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'NikeDunk' , color: 'verde', sku:"SKU: BST-002"},
+    {id: 89, nombre:"Nike Dunk High Orange", precio: 62999, img: "./imagenes/imagenes-zapatillas/nikedunkhighorange.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'NikeDunk' , color: 'blanco', sku:"SKU: BST-003"},
+    {id: 90, nombre:"Nike Dunk Kasina", precio: 29999, img: "./imagenes/imagenes-zapatillas/nikedunkkasina.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'NikeDunk' , color: 'azul', sku:"SKU: BST-006"},
+    {id: 91, nombre:"Nike Dunk Michigan", precio: 71999, img: "./imagenes/imagenes-zapatillas/nikedunkmichigan.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'NikeDunk' , color: 'azul', sku:"SKU: BST-007"},
+    {id: 92, nombre:"Nike Dunk Puregrey", precio: 39999, img: "./imagenes/imagenes-zapatillas/nikedunkpuregrey.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'NikeDunk' , color: 'gris', sku:"SKU: BST-008"},
+    {id: 93, nombre:"Nike Dunk Unc", precio: 38999, img: "./imagenes/imagenes-zapatillas/nikedunkunc.jpg", category: 'Zapatillas', marca: 'Jordan', modelo:'NikeDunk' , color: 'celeste', sku:"SKU: BST-009"},
+    {id: 93, nombre:"Nike Dunk Union LA", precio: 42999, img: "./imagenes/imagenes-zapatillas/nuikedunkunionla.webp", category: 'Zapatillas', marca: 'Jordan', modelo:'NikeDunk' , color: 'blanco', sku:"SKU: BST-010"},
 ];
+
+
+
+
 
 
 
@@ -64,7 +111,12 @@ productos.forEach((producto) => {
     <button class="botonStyle" onclick="verProducto(${producto.id})">Ver producto</button>
     </div>
     </div>`;
+    const carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
+    document.getElementById("sumarCarrito").innerHTML = carrito.length;
+    const total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    document.getElementById("sumarCarrito").innerHTML = `${carrito.length} - $${total}`;
 });
+
 
 /* funcion para eliminar productos del carrito */
 
@@ -85,9 +137,6 @@ function eliminarProducto(idDelProducto) {
     localStorage.setItem("carrito", JSON.stringify(carrito));
     window.location.reload();
 }
-
-
-
 
 
 
@@ -116,6 +165,43 @@ function filtrarProductosPorCategoria(categoria){
         </div>`; 
     });
 }
+
+/* funcion para filtrar zapatillas por modelos */
+
+for (const nodoHTML of document.getElementsByClassName('filtrar-zapatilla')){
+    nodoHTML.onclick = (event) => {
+        const model = (event.target.getAttribute('data-modelo'));
+        filtrarZapatillasPorModelo(model)
+    }
+}
+
+function filtrarZapatillasPorModelo(model){
+    document.getElementById("cards").innerHTML = "";
+    const productosFiltrados = productos.filter((producto) => producto.modelo === model);
+    productosFiltrados.forEach((producto) => {
+        document.getElementById("cards").innerHTML +=
+        `<div class='card' style='width: 16rem;'>
+        <img src="${producto.img}" class='card-img-top btn' alt='card imagen'>
+        <div class='card-body'>
+        <h5 class='card-title text-center'>${producto.nombre}</h5>
+        <p class="card-text text-center"><b>$${producto.precio}</b></p>
+        <button class="botonStyle" onclick="verProducto(${producto.id})">Ver producto</button>
+        </div>
+        </div>`;
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* funciones para filtrar los productos por marca */
