ce2c9758433244a5a167c1e8dab8652ce16d9208
Manuel Latorre <manuel.latorre11@gmail.com>
2022-09-01 11:12:01 -0300

    agregando talles a los productos

--- producto.js
+++ producto.js
@@ -1,4 +1,5 @@
 if (document.title === "Productos"){
+    
 
     const productoAMostrar = JSON.parse(localStorage.getItem('verProducto'));
     document.getElementById("nombre").innerHTML = productoAMostrar.nombre;
@@ -6,9 +7,10 @@ if (document.title === "Productos"){
     document.getElementById("seccion-imagenes").innerHTML += `<img class="card-img-top mb-5 mb-md-0" src="./${productoAMostrar.img}" alt="imagen">`
     document.getElementById("descripcion").innerHTML = productoAMostrar.descripcion;
     document.getElementById("sku").innerHTML = productoAMostrar.sku;
+
     
     const carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
-    document.getElementById("sumarCarrito").innerHTML = carrito.length;
+    
     const total = carrito.reduce((acumulador, productoAMostrar) => acumulador + productoAMostrar.precio, 0);
     document.getElementById("sumarCarrito").innerHTML = `${carrito.length} - $${total}`;
     
@@ -16,7 +18,7 @@ if (document.title === "Productos"){
     
     
     /*funcion para agregar al carrito  */
-
+    
     async function validarStock(id) {
         console.log(id)
         await fetch("productos.json")
@@ -31,25 +33,41 @@ if (document.title === "Productos"){
                 
                 Swal.fire({
                     title: 'Agregaste al carrito:',
-                text: productoAMostrar.nombre,
-                imageUrl: productoAMostrar.img,
-                imageWidth: 200,
-                imageHeight: 200,
-                imageAlt: 'Custom image',
-            })
-        }
+                    text: productoAMostrar.nombre,
+                    imageUrl: productoAMostrar.img,
+                    imageWidth: 200,
+                    imageHeight: 200,
+                    imageAlt: 'Custom image',
+                })
+            }
         else {Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: '¡Lo sentimos! No hay stock de este producto',
-                })
-            }
-        })
-    }
+            })
+        }
+    })
+}
 
     
     document.getElementById("agregarAlCarrito").addEventListener('click', () => {
         validarStock(productoAMostrar);
-        }
-    )
+    })
+
+
+
+
+    /* añadiendo tabla de talles al html */
+
+
+    document.getElementById("tabla-talles").innerHTML = `
+    <button class="btn btn-dark botonTalle">${productoAMostrar.talle1}</button>
+    <button class="btn btn-dark botonTalle">${productoAMostrar.talle2}</button>
+    <button class="btn btn-dark botonTalle">${productoAMostrar.talle3}</button>
+    <button class="btn btn-dark botonTalle">${productoAMostrar.talle4}</button>
+    <button class="btn btn-dark botonTalle">${productoAMostrar.talle5}</button>
+    <button class="btn btn-dark botonTalle">${productoAMostrar.talle6}</button>
+    `
 }
+
+
